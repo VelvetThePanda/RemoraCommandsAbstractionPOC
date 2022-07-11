8efddaf815cae31aadf015d8d8b81239a903b432
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-07-11 09:47:48 -0400

    Initial commit of the POC

---
+++ RemoraCommandsAbstractionPOC/CodeWriter.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace RemoraCommandsAbstractionPOC;
+
+public class CodeWriter : IDisposable
+{
+    private readonly StringBuilder _stringBuilder = new();
+    private readonly CodeWriter? _parent;
+    private readonly int _indent = 0;
+
+    public CodeWriter() { }
+    
+    private CodeWriter(CodeWriter parent)
+    {
+        _parent = parent;
+        _indent = parent._indent + 1;
+    }
+    
+    public CodeWriter Append(string text)
+    {
+        _stringBuilder.Append(text);
+        
+        return this;
+    }
+    
+    public CodeWriter Append(char text)
+    {
+        _stringBuilder.Append(text);
+        
+        return this;
+    }
+
+    public CodeWriter AppendLine(char text)
+    {
+        _stringBuilder.AppendLine(text.ToString());
+
+        return this;
+    }
+    
+    public CodeWriter AppendLine(string text)
+    {
+        _stringBuilder.AppendLine(text);
+        
+        return this;
+    }
+
+    public CodeWriter Flush()
+    {
+        _parent?.AppendLine(ToString());
+        _stringBuilder.Clear();
+
+        return this;
+    }
+    
+    public CodeWriter CreateChildWriter() => new(this);
+
+    public override string ToString() => _stringBuilder.ToString();
+
+    private string GetIndent() => new('\t', _indent);
+
+    public void Dispose() => Flush();
+
+
+}
---
+++ RemoraCommandsAbstractionPOC/CommandShimGenerator.cs
@@ -0,0 +1,213 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RemoraCommandsAbstractionPOC;
+
+[Generator]
+public class CommandShimGenerator : ISourceGenerator
+{
+    private const string CommandGroupName = ": CommandGroup";
+
+    public void Initialize(GeneratorInitializationContext context) { }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        foreach (var tree in context.Compilation.SyntaxTrees)
+        {
+            var builder = new CodeWriter();
+
+            builder.AppendLine("// Auto-generated");
+            builder.AppendLine("using System;");
+            builder.AppendLine("using Remora.Results;");
+            builder.AppendLine("using Remora.Commands;");
+            builder.AppendLine("using System.Collections.Generic;");
+
+            var root = tree.GetRoot();
+
+            var namespaceDeclaration = root.DescendantNodes().OfType<NamespaceDeclarationSyntax>().First();
+
+            builder.AppendLine($"namespace {namespaceDeclaration.Name}");
+            builder.AppendLine('{');
+
+            using var classWriter = builder.CreateChildWriter();
+
+            foreach (var node in root.DescendantNodes())
+            {
+                if (node is ClassDeclarationSyntax cds)
+                {
+                    if (!cds.Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword)))
+                        continue;
+
+                    if (!cds.BaseList?.Types.Any() ?? true)
+                        break; // Class doesn't inherit anything.
+
+                    var commandGroup = context.Compilation.GetTypeByMetadataName("Remora.Commands.Groups.CommandGroup");
+
+                    var symbol = context.Compilation.GetSemanticModel(tree).GetDeclaredSymbol(cds);
+
+                    // Not a command group.
+                    if (!symbol.BaseType.Equals(symbol))
+                        break;
+
+                    classWriter.AppendLine($"class {cds.Identifier}");
+                    classWriter.AppendLine("{");
+                }
+
+                if (node is MethodDeclarationSyntax mds)
+                {
+                    var symbol = context.Compilation.GetSemanticModel(tree).GetDeclaredSymbol(mds);
+
+                    if (symbol.DeclaredAccessibility.HasFlag(Accessibility.Public))
+                        continue;
+
+                    // Check if the return type is a task of sorts
+                    if (symbol.ReturnType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) is not ("global::System.Threading.Tasks.Task" or "global::System.Threading.Tasks.ValueTask"))
+                        continue;
+
+                    // Check if the method has a command attribute
+                    var attributes = symbol.GetAttributes();
+
+                    var commandAttribute = context.Compilation.GetTypeByMetadataName("Remora.Commands.Attributes.CommandAttribute")!;
+
+                    if (!attributes.Any(a => a.AttributeClass.Equals(commandAttribute)))
+                        continue;
+
+                    using var methodWriter = classWriter.CreateChildWriter();
+
+                    RipAttributes(methodWriter, attributes);
+                    ShimMethod(methodWriter, symbol);
+                }
+
+                classWriter.AppendLine('}');
+            }
+
+            context.AddSource("RemoraCommandsAbstractionPOC.Generated", builder.ToString());
+
+        }
+    }
+
+    private static void ShimMethod(CodeWriter methodWriter, IMethodSymbol symbol)
+    {
+        methodWriter.Append("public async Task<Result> ");
+        methodWriter.Append(symbol.Name + "_Shim");
+
+        if (!symbol.Parameters.Any() || symbol.Parameters.All(p => !p.GetAttributes().Any()))
+        {
+            methodWriter.Append("()");
+        }
+        else
+        {
+            methodWriter.AppendLine('(');
+
+            using var parameterWriter = methodWriter.CreateChildWriter();
+
+            for (var i = 0; i < symbol.Parameters.Length; i++)
+            {
+                var parameter = symbol.Parameters[i];
+                RipAttributes(parameterWriter, parameter.GetAttributes());
+
+                if (parameter.IsParams)
+                {
+                    parameterWriter.Append("params ");
+                }
+
+                parameterWriter.Append(parameter.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat));
+
+                if (parameter.Type.TypeKind is TypeKind.Array)
+                {
+                    parameterWriter.Append("[]");
+                }
+
+                parameterWriter.Append(parameter.Name);
+
+                if (parameter.IsOptional)
+                {
+                    parameterWriter.Append(" = ");
+                    parameterWriter.Append(parameter.ExplicitDefaultValue.ToString());
+                }
+
+                if (symbol.Parameters.Length > 1 && i < symbol.Parameters.Length - 1)
+                    parameterWriter.Append(',');
+
+                parameterWriter.AppendLine(' ');
+            }
+
+            methodWriter.AppendLine(')');
+
+            methodWriter.AppendLine('{');
+
+            using var bodyWriter = methodWriter.CreateChildWriter();
+
+            bodyWriter.AppendLine($"await {symbol.Name}({string.Join(", ", symbol.Parameters.Select(p => p.Name))}");
+            bodyWriter.AppendLine(");");
+
+            bodyWriter.AppendLine("return Result.Success();");
+
+            methodWriter.AppendLine('}');
+        }
+    }
+
+    private static void RipAttributes(CodeWriter writer, ImmutableArray<AttributeData> attributes)
+    {
+        foreach (var attribute in attributes)
+        {
+            writer.Append('[');
+
+            var attributeName = attribute.AttributeClass.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+            writer.Append(attributeName);
+
+            if (attribute.ConstructorArguments.Any())
+            {
+                var stringifiedArguments = new List<string>();
+
+                foreach (var argument in attribute.ConstructorArguments)
+                {
+                    // Check if the argument is an enum
+                    if (argument.Value is ITypeSymbol typeSymbol)
+                    {
+                        if (typeSymbol.TypeKind == TypeKind.Enum)
+                        {
+                            var enumName = typeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+                            var enumValue = argument.Value.ToString();
+                            stringifiedArguments.Add($"{enumName}.{enumValue}");
+                        }
+                        else
+                        {
+                            stringifiedArguments.Add(argument.Value.ToString());
+                        }
+                    }
+                    else
+                    {
+                        stringifiedArguments.Add(argument.Value.ToString());
+                    }
+                }
+
+                foreach (var argument in attribute.NamedArguments)
+                {
+                    // Check if the argument is an enum
+                    if (argument.Value.Kind is TypedConstantKind.Enum)
+                    {
+                        var enumName = argument.Value.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+                        var enumValue = argument.Value.Value.ToString();
+                        stringifiedArguments.Add($"{argument.Key} = {enumName}.{enumValue}");
+                    }
+                    else
+                    {
+                        stringifiedArguments.Add($"{argument.Key} = {argument.Value.ToString()}");
+                    }
+                }
+
+                writer.Append('(');
+                writer.Append($"{attributeName}({string.Join(", ", stringifiedArguments)})");
+                writer.Append(')');
+            }
+
+            writer.AppendLine(']');
+        }
+    }
+}

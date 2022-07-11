ae797649f23db644f369a71195dca5bf558d10b1
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-07-11 11:00:35 -0400

    Add test class and project

---
+++ TestCommandsAssembly/TestClasses.cs
@@ -0,0 +1,15 @@
+﻿using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Results;
+
+namespace TestCommandsAssembly;
+
+public class TestClasses : CommandGroup
+{
+    [Command("ooga")]
+    async Task OogaCommand() => await Task.CompletedTask;
+    
+    [Command("booga")]
+    public Task<Result> BoogaCommand() => Task.FromResult(Result.FromSuccess());
+    
+}
